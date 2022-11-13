0a026b0f597540a34e72687e7736c4dfcdfc12a2
Yernazar <rakernazar@gmail.com>
2022-11-13 22:27:28 +0600

    second Post method

--- project-with-tabs-timer-modal/js/script.js
+++ project-with-tabs-timer-modal/js/script.js
@@ -144,7 +144,9 @@ window.addEventListener('DOMContentLoaded', function(){
     };
 
     let form = document.querySelector('.main-form'),
+        form2 = document.querySelector('#form'),
         input = form.getElementsByTagName('input'),
+        input2 = form2.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
     statusMessage.classList.add('status');
@@ -180,7 +182,41 @@ window.addEventListener('DOMContentLoaded', function(){
         });
 
         for(let i = 0; i < input.length; i++) {
-            input.value = '';
+            input[i].value = '';
+        };
+    });
+    form2.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form2.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form),
+            obj = {};
+        
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            }else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            }else{
+                statusMessage.innerHTML = message.failure;
+            };
+        });
+
+        for(let i = 0; i < input.length; i++) {
+            input2[i].value = '';
         };
     });
 });
